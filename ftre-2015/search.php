04184ad5b82fc4a11fa672b0b1b3825abd69d8e5
Noel Slevin <noelslevin@gmail.com>
2015-11-13 23:21:07 +0000

    Search page
    Added search form and sidebar to the search page.

--- ftre-2015/search.php
+++ ftre-2015/search.php
@@ -5,17 +5,22 @@
 		<header class="primary-header">
 		<?php if ( have_posts() ) : ?>
 			<h1><?php printf( ( 'Search Results for: %s'), get_search_query()); ?></h1>
+            </header>
 			<?php while ( have_posts() ) : the_post(); ?>
 			<h2><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
 			<?php the_excerpt(); ?>
 			<?php endwhile; ?>
-		</header>
 		<?php else : ?>
+        <h1>Nothing found</h1>
+            </header>
+        <p>Try searching for something else instead.</p>
+        <?php get_search_form(); ?>
 		
-		</header>
 		<?php endif; ?>
 		
 	</div>
+
+<?php get_sidebar(); ?>
 </div>
 
 <?php get_footer(); ?>
