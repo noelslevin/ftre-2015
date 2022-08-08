dfa709800d7bb88bd4eadbc818f4800f01154440
Noel Slevin <noelslevin@gmail.com>
2022-08-08 23:44:44 +0100

    Update header.php
    Updated header logo and improved site metadata for SEO.

--- ftre-2015/header.php
+++ ftre-2015/header.php
@@ -3,6 +3,14 @@
   <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>" />
     <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1">
+	<meta property="og:image" content="<?php echo get_stylesheet_directory_uri(); ?>/images/ftre-og-image.png"/>
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="twitter:site" content="@watfordpodcast" />
+	<meta name="twitter:title" content="From the Rookery End" />
+	<meta name="twitter:description" content="A podcast by football fans, for football fans." />
+	<meta name="twitter:creator" content="@watfordpodcast">
+    <meta name="twitter:image:alt" content="FTRE Logo">
+    <meta name="twitter:domain" content="fromtherookeryend.com">
     <title><?php wp_title( '|', true, 'right' ); ?></title>
     <link rel="profile" href="http://gmpg.org/xfn/11" />
     <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
@@ -16,7 +24,7 @@
             <?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?>
           <a href="/" class="header-link">
               <div class="ftre-logo">
-                <img class="header-img" src="<?php echo get_stylesheet_directory_uri(); ?>/images/ftre-logo.png" alt="From the Rookery End" />
+                <img class="header-img" src="<?php echo get_stylesheet_directory_uri(); ?>/images/ftre-2018.png" alt="From the Rookery End" />
                   <h1 class="hidden"><?php bloginfo('name'); ?></h1>
               </div>
           </a>
