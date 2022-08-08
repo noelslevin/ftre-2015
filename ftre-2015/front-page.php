12957a757681e06fd8a59ccaf23b48a1fdbb68a3
Noel Slevin <noelslevin@gmail.com>
2022-08-08 23:46:31 +0100

    Update front-page.php
    Removed old comments and updated podcast section.

--- ftre-2015/front-page.php
+++ ftre-2015/front-page.php
@@ -3,7 +3,11 @@
 <div class="site-content">
     
     <div class="home-page-section">
-	<?php getLatestPod('https://audioboom.com/channels/4225404.rss', 'ftre-premier-league.jpg', 'https://feeds.feedburner.com/rookeryend', 'https://itunes.apple.com/gb/podcast/from-the-rookery-end/id374798846'); ?> 
+	<?php getLatestPod('https://audioboom.com/channels/4225416.rss', 'ftre-premier-league.jpg', 'https://feeds.feedburner.com/rookeryend', 'https://itunes.apple.com/gb/podcast/from-the-rookery-end/id374798846'); ?>
+	<div class="podcast-links right">
+		<a href="https://www.google.com/podcasts?feed=aHR0cHM6Ly9hdWRpb2Jvb20uY29tL2NoYW5uZWxzLzQyMjU0MTYucnNz"><img class="inline-svg" src="<?php echo get_stylesheet_directory_uri(); ?>/images/google-podcasts.svg" alt="Listen on Google Podcasts"></a>
+		<a href="https://itunes.apple.com/gb/podcast/from-the-rookery-end-watford-fc/id374798846?mt=2"><img class="inline-svg" src="<?php echo get_stylesheet_directory_uri(); ?>/images/apple-podcasts.svg" alt="Listen on Apple Podcasts"></a>
+		</div>
 	</div>
     
     <div class="home-page-section">
@@ -11,10 +15,6 @@
         <div class="stanley-grouse">
             <?php getArtwork(); ?>
         </div>
-	</div>
-    
-	<div class="home-page-section">
-		<?php getLatestPod('https://audioboom.com/channels/1472379.rss', 'ftre-shorts.jpg', 'https://feeds.feedburner.com/rookeryend', 'https://itunes.apple.com/gb/podcast/from-the-rookery-end/id374798846'); ?>
 	</div>
 	
 </div>
