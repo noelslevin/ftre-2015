820a001ac33c395438791ee350b6114ee038f99b
Noel Slevin <noelslevin@gmail.com>
2022-08-08 23:44:21 +0100

    Update footer.php
    Updated copyright text and cleaned up old comments.

--- ftre-2015/footer.php
+++ ftre-2015/footer.php
@@ -1,8 +1,8 @@
 	<footer class="center">
         <p><?php bloginfo('description'); ?></p>
-        <p>&copy; 2008-<?php echo date('Y'); ?> Jon Moonie, Mike Parkin &amp; Jason Bailey.</p>
+        <p>&copy; <?php echo date('Y'); ?> Jon Moonie, Mike Parkin &amp; Jason Bailey.</p>
 	</footer>
   <?php wp_footer(); ?>
-</div>
+
   </body>
 </html>
