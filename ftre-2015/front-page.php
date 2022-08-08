787d2d14d721b87022caacee732cafc41b3baded
Noel Slevin <noelslevin@gmail.com>
2022-08-09 00:08:04 +0100

    Added Spotify links
    Added Spotify icon, a podcast link and the associated styles.

--- ftre-2015/front-page.php
+++ ftre-2015/front-page.php
@@ -7,6 +7,7 @@
 	<div class="podcast-links right">
 		<a href="https://www.google.com/podcasts?feed=aHR0cHM6Ly9hdWRpb2Jvb20uY29tL2NoYW5uZWxzLzQyMjU0MTYucnNz"><img class="inline-svg" src="<?php echo get_stylesheet_directory_uri(); ?>/images/google-podcasts.svg" alt="Listen on Google Podcasts"></a>
 		<a href="https://itunes.apple.com/gb/podcast/from-the-rookery-end-watford-fc/id374798846?mt=2"><img class="inline-svg" src="<?php echo get_stylesheet_directory_uri(); ?>/images/apple-podcasts.svg" alt="Listen on Apple Podcasts"></a>
+		<a href="https://open.spotify.com/show/0UUCvUimZxjhgVPiY5Mvlh"><img class="inline-svg spotify" src="<?php echo get_stylesheet_directory_uri(); ?>/images/spotify-podcast-badge.svg" alt="Listen on Spotify"></a>
 		</div>
 	</div>
     
