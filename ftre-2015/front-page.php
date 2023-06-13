d47907329189feebe3349b4b371f2bcb23a83ee6
Noel Slevin <noelslevin@gmail.com>
2023-06-13 23:03:25 +0100

    Update front-page.php
    Swapped the Audioboom link for Acast.

--- ftre-2015/front-page.php
+++ ftre-2015/front-page.php
@@ -3,7 +3,7 @@
 <div class="site-content">
     
     <div class="home-page-section">
-	<?php getLatestPod('https://audioboom.com/channels/4225416.rss', 'ftre-premier-league.jpg', 'https://feeds.feedburner.com/rookeryend', 'https://itunes.apple.com/gb/podcast/from-the-rookery-end/id374798846'); ?>
+	<?php getLatestPod('https://feeds.acast.com/public/shows/628e15899f8b3900138cb94a', 'ftre-premier-league.jpg', 'https://feeds.feedburner.com/rookeryend', 'https://itunes.apple.com/gb/podcast/from-the-rookery-end/id374798846'); ?>
 	<div class="podcast-links right">
 		<a href="https://www.google.com/podcasts?feed=aHR0cHM6Ly9hdWRpb2Jvb20uY29tL2NoYW5uZWxzLzQyMjU0MTYucnNz"><img class="inline-svg" src="<?php echo get_stylesheet_directory_uri(); ?>/images/google-podcasts.svg" alt="Listen on Google Podcasts"></a>
 		<a href="https://itunes.apple.com/gb/podcast/from-the-rookery-end-watford-fc/id374798846?mt=2"><img class="inline-svg" src="<?php echo get_stylesheet_directory_uri(); ?>/images/apple-podcasts.svg" alt="Listen on Apple Podcasts"></a>
